1f129849a085072e6d0902ffdbad637899e58029
emmanuellaisa00 <emmanuellaisa00@gmail.com>
2026-05-30 23:19:12 +0300

    Upload via GitFlow

--- app/src/main/java/com/beatdrop/kt/MainActivity.kt
+++ app/src/main/java/com/beatdrop/kt/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.material.icons.outlined.Explore
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.ui.unit.dp
 import androidx.compose.material3.Text
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
